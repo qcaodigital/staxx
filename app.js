01bca61d5f374d44e36feeb8e518291c3e21cd0e
qcaodigital <qcao.digital@gmail.com>
2021-01-27 14:47:48 -0500

    disable livereload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
+// const path = require('path');
+// const livereload = require("livereload");
+// const connectLivereload = require("connect-livereload");
 
-// open livereload high port and start to watch public directory for changes
-const liveReloadServer = livereload.createServer({ exts: ['ejs', 'css', 'js'] });
-liveReloadServer.watch([__dirname + '/public', __dirname + '/views']);
-app.use(connectLivereload());
+// // open livereload high port and start to watch public directory for changes
+// const liveReloadServer = livereload.createServer({ exts: ['ejs', 'css', 'js'] });
+// liveReloadServer.watch([__dirname + '/public', __dirname + '/views']);
+// app.use(connectLivereload());
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://qcaodigital:${process.env.MONGO_PW}@qcaodigital.vys9n.mongodb.net/qcaodigital?retryWrites=true&w=majority`;
@@ -38,9 +38,9 @@ client.connect(async(err) => {
     app.listen(port, () => console.log('Server started on port', port))
 
     // ping browser on Express boot, once browser has reconnected and handshaken
-    liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-        liveReloadServer.refresh("/");
-    }, 100);
-    });
+    // liveReloadServer.server.once("connection", () => {
+    // setTimeout(() => {
+    //     liveReloadServer.refresh("/");
+    // }, 100);
+    // });
 });
